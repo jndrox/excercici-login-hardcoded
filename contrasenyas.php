6d816a06b5872a543f519a9ba0d6b12074852e1b
jndrox <74646811+jndrox@users.noreply.github.com>
2020-11-26 22:26:39 +0100

    Add files via upload

---
+++ contrasenyas.php
@@ -0,0 +1,8 @@
+<?php
+     $password=array(
+        1=>password_hash("iloveu",PASSWORD_BCRYPT),
+        2=>password_hash("m4k3Am3r1caGr3atAg41n!",PASSWORD_BCRYPT),
+        3=>password_hash("ErF4ryS1empr3",PASSWORD_BCRYPT),
+        4=>password_hash("Fatality!",PASSWORD_BCRYPT),
+    );
+    
